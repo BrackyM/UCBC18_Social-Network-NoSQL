ff009ba0772ba96f2e76ddee2ee9b88d7b16e753
BraxtonM <braxtoncoding@gmail.com>
2023-01-04 20:55:01 -0500

    Completed most of the models for this exercise

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const reactSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'You must input a Thought',
+            minlength: 1,
+            maxlength: 300
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+const User = model('User', userSchema);
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: // figure out matching with regex email
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+
+
+module.exports = User;
